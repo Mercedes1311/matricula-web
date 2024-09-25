c3856652084ce8f9b5e4d40847894df49cd49c1e
Herber215 <herber1598@gmail.com>
2024-09-25 13:52:38 -0500

    Primero

--- App/models.py
+++ App/models.py
@@ -7,3 +7,17 @@ class Usuario(AbstractUser):
 
     def __str__(self):
         return self.usuario
+    
+class Alumno(models.Model):
+    codigo_alumno = models.CharField(max_length=10, unique=True)
+    nombres = models.CharField(max_length=100)
+    apellido_paterno = models.CharField(max_length=100)
+    apellido_materno = models.CharField(max_length=100)
+    plan = models.CharField(max_length=50)
+    dni = models.CharField(max_length=8)
+    correo = models.EmailField()
+    celular = models.CharField(max_length=15)
+    escuela = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.codigo_alumno} - {self.nombres} {self.apellido_paterno}'
