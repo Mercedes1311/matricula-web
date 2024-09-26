7cd7845661aa2c81ff04b2c3e0cc390589e5abdd
KiaraLuz <fernandezperezkiara@gmail.com>
2024-09-25 21:02:17 -0500

    login
    estoy cansado jefe

--- matriWeb/urls.py
+++ matriWeb/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for matriWeb project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from App import views
@@ -22,7 +6,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"),
     path('registro/', views.registro, name='registro'),
-    path('matricula/', views.matricula_view, name='matricula_view'),
     path('signin/', views.signin, name="signin"),
     path('signout/', views.signout, name='signout'),
+    path('usuario/<str:username>/', views.perfil, name='perfil'),
 ]
